36192b5ff064faf66bac6c465d33a35af062c214
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-31 17:53:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-// Code your solution here!
+function printString(myString) {
+    console.log(myString[0]);
+  
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
 
+function reverseString(myString){ 
+    if (myString.length > 1) {
+        return reverseString(myString.substr(1)) + myString.charAt(0)
+    } else {
+      return myString;
+    }
+  }
+
+function isPalindrome(string){
+    if (string === reverseString(string.substr(1)) + string.charAt(0))
+        return true
+    else
+        return false
+}
+
+function addUpTo(arr, i){
+    
+    
+}
+
+function maxOf(arr){
+    let nums = arr.slice();
+
+    if (nums.length == 1) { return nums[0]; }
+
+    if (nums[0] < nums[1]) { nums.splice(0,1); }
+    else { nums.splice(1,1); }
+
+    return maxOf(nums);
+}
+
+function includesNumber(arr, num){
+    let nums = arr.slice()
+    
+}
