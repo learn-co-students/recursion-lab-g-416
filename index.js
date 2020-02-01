81801ed6fbe25caf851be8ba34e1e4fb6bf42c97
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-31 17:13:51 -0700

    reverseString working

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function printString(str) {
       return console.log(str[0])
    }
 }
-
-printString(myString)
-
 // // End goal = " p i z z a "
 // pizza 
 // str.length = 5
@@ -29,4 +26,32 @@ printString(myString)
 
 // What's the stopping point? (base case?)
 // In this case its the end of the string;  this is what my conditional should be based on. 
+
+
+//printString(myString) -> Works! 
+
+// 1. apply the problem to a specific use case - "pizza" string
+// 2. write a function that manually can get the solution for this specific example
+// function reverseString(str) {
+//    return str[str.length - 1] + str[str.length - 2] + str[str.length - 3] + str[str.length - 4] + str[str.length - 5]
+// }
+// 3. Reword that function so that it uses recursion, invoking itself. 
+// "print out the last character in the string, then recursively call yourself on the last character in the substring until there are no more characters. 
+// 4. write the function so it will do this recursively:
+
+function reverseString(str) {
+   let result = ""
+   if (str.length > 1) {
+      result = str[str.length - 1] + reverseString(str.substring(0, str.length - 1))
+
+   } else {
+      return str[0]
+   }
+   return result
+}
+
+console.log(reverseString("pizza"))
+
+
+
 
