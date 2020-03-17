06d7b17f21ed28526195274edbec2b7403378280
Doug Kintop <dkintop@hotmail.com>
2020-03-17 15:21:46 -0400

    Done.

--- index.js
+++ index.js
@@ -34,3 +34,21 @@ function isPalindrome(myString) {
 function addUpTo(myArray, index) {
   return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index];
 }
+
+function maxOf(myArray) {
+  if (myArray.length === 1) {
+    return myArray[0];
+  } else {
+    return Math.max(myArray.pop(), maxOf(myArray));
+  }
+}
+
+function includesNumber(myArray, myNumber) {
+  if (!myArray.length) {
+    return false;
+  } else if (myArray[0] === myNumber) {
+    return true;
+  } else {
+    return includesNumber(myArray.slice(1), myNumber);
+  }
+}
