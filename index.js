0a0eac62a70a26273f0911fe0219e1644e2a3a89
jessesbyers <jesse.s.byers@gmail.com>
2020-04-22 20:22:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-// Code your solution here!
+function printString(myString) {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
 
+  function reverseString(string) {
+    if (string) {
+        return reverseString(string.substr(1)) + string[0]
+    } else {
+        return string
+    }
+  }
+
+  function isPalindrome(string) {
+      if (string === reverseString(string)) {
+        return true 
+      } else {
+        return false
+      }
+  }
