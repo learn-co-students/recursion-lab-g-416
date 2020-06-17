abca43b2ca50938c1ab262ccf4a953f230030256
Dominique Skaf <doskaf@yahoo.com>
2020-06-17 18:43:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-// Code your solution here!
+function printString(myString) {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
 
+  function reverseString(myString) {
+      let myReversedString = ""
+      myReversedString.concat(myString[myString.length - 1]);
+
+      if (myString.length > 1) {
+        let mySubString = myString.substring(0, myString.length - 1);
+        reverseString(mySubString);
+      } else {
+          return myReversedString;
+      }
+  }
