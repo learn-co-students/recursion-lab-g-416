f58e3be7c96dc3cfebc2a850a746970cc588bc2d
Dominique Skaf <doskaf@yahoo.com>
2020-06-18 12:23:10 -0500

    Done.

--- index.js
+++ index.js
@@ -28,3 +28,36 @@ function printString(myString) {
         return true
     }
   }
+
+  function addUpTo(myArray, myIndex) {
+    if (myIndex > 0) {
+        return myArray[0] + addUpTo(myArray.slice(1), myIndex - 1)
+    } else {
+        return myArray[0]
+    }
+  }
+
+  function maxOf(myArray) {
+    if (myArray.length > 1) {
+      let maxOfSubArray = maxOf(myArray.slice(1))
+      if (myArray[0] > maxOfSubArray) {
+        return myArray[0]
+      } else {
+        return maxOfSubArray
+      }
+    } else {
+      return myArray[0]
+    }
+  }
+
+  function includesNumber(myArray, myNum) {
+    if (myArray.length > 1 ) {
+      if (myArray[0] !== myNum) {
+        return includesNumber(myArray.slice(1), myNum)
+      } else {
+        return true
+      }
+    } else {
+      return (myArray[0] === myNum)
+    }
+  }
