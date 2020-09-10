7a8a9866247779011f6773a0e984386287524d94
Amy Westcott <amy.westcott@yahoo.com>
2020-09-10 14:30:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-// Code your solution here!
+function printString(myString) {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
 
+  function reverseString(string){
+    if (string.length > 0) {
+        console.log(reverseString(string.substring(1)) + string[0])
+    } else if (string === "") {
+        console.log(string)
+    }
+}
+
+  
