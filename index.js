5cbc7c47e53ed761e984e7b66b47e34f893573c0
allisoncortez <allizetorc@gmail.com>
2020-10-12 07:43:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-// Code your solution here!
+function printString(myString) {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
+
+//STRING CHALLENGES
+//1. reverse a string
+//2. see if a word is a palindrome..
+
+//ARRAY CHALLENGES
+// 1. Given an array and an index, write a recursive function to add up the elements of an array.
+// 2. Write a recursive function to find the largest integer in an array.
+// 3. Write out a function to see if an array includes a given element.
 
