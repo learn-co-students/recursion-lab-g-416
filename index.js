f646b8e75dffa2f42d9fa2f218e69597ae9f3e41
Wendy Harris <wendyv510@hotmail.com>
2020-11-13 14:52:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 // Code your solution here!
 
+function printString(myString){
+    console.log(myString[0]);
+
+    if (myString.length > 1){
+        let mySubString = myString.substring(1, myString.length);
+        printString(mySubString);
+    } else {
+        return true; 
+    }
+}
+
+function reverseString(myString){
+    console.log(myString[0]);
+
+    if(myString.length < 1){
+        return myString 
+    } else {
+        return reverseString(myString.substring(1)) + myString[0]
+    }
+}
+
+function isPalindrome(inputString){
+    if (inputString == inputString.split('').reverse().join('')){
+        return true 
+    } else {
+        return false 
+    }
+}
