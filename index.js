9122e014407e763341296c57de08e068d3281108
Wendy Harris <wendyv510@hotmail.com>
2020-11-16 13:40:49 -0500

    Done.

--- index.js
+++ index.js
@@ -21,10 +21,43 @@ function reverseString(myString){
     }
 }
 
-function isPalindrome(inputString){
-    if (inputString == inputString.split('').reverse().join('')){
-        return true 
+function isPalindrome(myString) {
+    if(myString.slice(0,1) !== myString.slice(-1)) {
+      return false
+    } else if (myString.length > 1) {
+      return isPalindrome(myString.slice(1, -1))
     } else {
-        return false 
+      return true
     }
-}
+  }
+  
+  function addUpTo(arr, i) {
+    if(i > 0) {
+      return arr[0] + addUpTo(arr.slice(1), i - 1)
+    } else {
+      return arr[0]
+    }
+  }
+  
+  function maxOf(arr) {
+    if(arr.length > 1) {
+      let maxRest = maxOf(arr.slice(1))
+      if(arr[0] > maxRest) {
+        return arr[0]
+      } else {
+        return maxRest
+      }
+    } else {
+      return arr[0]
+    }
+  }
+  
+  function includesNumber(arr, num) {
+    if(arr[0] == num) {
+      return true
+    } else if (arr.length > 0) {
+      return includesNumber(arr.slice(1), num)
+    } else {
+      return false
+    }
+  }
