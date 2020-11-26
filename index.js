ade507261b18c744de475e8fa3b936e0d0bd701d
agiletkiewicz <a.letkiewicz@gmail.com>
2020-11-26 10:48:51 -0800

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,49 @@ function printString(myString) {
       return true;
     }
   }
+
+  function reverseString(myString) {
+    if (myString.length < 2) {
+      return myString;
+    } else {
+      return reverseString(myString.substring(1)) + myString[0];
+    }
+  }
+
+  function isPalindrome(myString) {
+    let l = myString.length;
+  
+    if (l < 2) {
+      return true;
+    } else if (myString[l - 1] === myString[0]) {
+      return isPalindrome(myString.substring(1, l - 1));
+    } else {
+      return false;
+    }
+  }
+
+  function addUpTo(array, index) {
+    if (index > 0) {
+      return addUpTo(array, index - 1) + array[index]
+    } else {
+      return array[0]
+    }
+  }
+
+  function maxOf(myArray) {
+    if (myArray.length === 1) {
+      return myArray[0];
+    } else {
+      return Math.max(myArray.pop(), maxOf(myArray));
+    }
+  }
+
+  function includesNumber(array, element) {
+    if (array[0] === element) {
+      return true;
+    } else if (array.length === 1){
+      return false;
+    } else {
+      return includesNumber(array.slice(1), element);
+    }
+  }
