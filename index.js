e4d0303f8057534a480534b60b8ac14e5308a7ca
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-11-28 03:10:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string){
+  console.log(string[-1])
+
+  if (string.length > 1) {
+    let mySubString = string.substring(1, string.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
