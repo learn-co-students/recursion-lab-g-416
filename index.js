b6beca6b2d4cdda86a5ce5c96d7727d7bc79f4f8
Nashmeyah <nashalrekabi@gmail.com>
2021-02-03 22:25:25 -0700

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function printString(myString) {
 }
 
 function reverseString(string) {
-  if (string == "") {
+  if (string === "") {
     return "";
   } else {
     return reverseString(string.substr(1)) + string.charAt(0);
@@ -19,15 +19,26 @@ function reverseString(string) {
 }
 
 function isPalindrome(string) {
-  if (string === reverseString(string)) {
-    return true;
-  } else {
+  if (string.length <= 1) return true;
+
+  if (string[0] !== string[string.length - 1]) {
     return false;
   }
+
+  return isPalindrome(string.substr(1, string.length - 2));
 }
 
 function includesNumber(array) {}
 
-function addUpTo(array, index) {}
+function addUpTo(array, index) {
+  //take index number
+  // have a count
+  // count + indexOf(index)
+  // index - 1
+  // count + indexOf(index)
+
+  let sum = 0;
+  sum += indexOf(index);
+}
 
 function maxOf(nums) {}
