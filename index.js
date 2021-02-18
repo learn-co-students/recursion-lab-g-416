a523fb89ba59a0957a4e66f98029f93740ce357b
Uma Manandhar <um_2003_8@hotmail.com>
2021-02-18 21:06:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ function addUpTo(myArray, index) {
   return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index]
 }
 
-function maxOf(myArray) {
-
+function maxOf(array) {
+  if (array.length == 1) {
+    return array[0]
+  } else {
+    return Math.max(array.pop(), maxOf(array))
+  }
 }
