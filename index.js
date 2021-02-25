d3c4b8b67e9e05c2a1d8fc324e687632b18f99d9
Olesya Miller <olesyam.miller@gmail.com>
2021-02-24 22:36:19 -0400

    Done.

--- index.js
+++ index.js
@@ -17,9 +17,19 @@ function reverseString(myString) {
     } else {
       return reverseString(myString.substring(1)) + myString[0];
     }
-  }
+}
 
-
+function isPalindrome(word) {
+    // return word === reverseString(word) ? true : false 
+        if(word.length < 2 || (word[0] === word[1] && word.length === 2)){
+            return true
+        } else if (word[0] === word[word.length - 1]){
+            let remainder = word.substring(1, word.length - 1)
+            return isPalindrome(remainder)
+        } else {
+          return false 
+        }
+}
 
 
 
