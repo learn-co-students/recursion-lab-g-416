33d1e21bba2a61ce9a082f7da8d5c1b78380f2ec
Olesya Miller <olesyam.miller@gmail.com>
2021-02-24 22:04:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // Code your solution here!
 
+function printString(string) {
+    console.log(string[0])
+
+    if (string.length > 1) {
+        let mySubstring = string.substring(1, string.length)
+        printString(mySubstring)
+    } else {
+        return true 
+    }
+}
+
+function reverseString(myString) {
+    if (myString.length < 2) {
+      return myString;
+    } else {
+      return reverseString(myString.substring(1)) + myString[0];
+    }
+  }
+
+
+
+
+
