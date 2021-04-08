ae266422d86022a83776af71ee5d1909b349a849
christinezosche <christine.zosche@gmail.com>
2021-04-08 11:36:09 -0700

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ function printString(myString) {
       return true;
     }
   }
+
+  function reverseString(myString) {
+      let newString = []
+      function reverse(string) {
+        if (string.length > 1) {
+        let letter = string[string.length - 1]
+        let remainder = string.substring(0, string.length - 1)
+        newString.push(letter)
+        reverse(remainder)
+        }
+    else {
+      newString.push(string)
+    }
+  }
+  reverse(myString)
+    console.log(newString.join(''))
+
+  }
