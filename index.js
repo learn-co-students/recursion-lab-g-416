c554926fa7f75108c30aea982a7ab74ef513bbd5
christinezosche <christine.zosche@gmail.com>
2021-04-08 10:25:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // Code your solution here!
-function printString(string) {
-    let substring;
+function printString(myString) {
+    console.log(myString[0]);
    
-    // Print out the current first character in the string.
-    console.log(string[0]);
-   
-    // Store the remainder of the string in the 'substring' variable.
-    substring = string.substring(1, string.length);
-   
-    // Invoke printString() from within, passing in the remainder of the previous string.
-    printString(substring);
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
   }
-   
-
